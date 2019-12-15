bdadf27a2f92cd0029788015e1534ebd45e0ccb6
FanJups <jupsfan@gmail.com>
2019-12-15 22:37:37 +0100

    Adding Simple Test

--- src/test/java/io/hwc/enad/EnadApplicationTests.java
+++ src/test/java/io/hwc/enad/EnadApplicationTests.java
@@ -1,13 +1,26 @@
 package io.hwc.enad;
 
-import org.junit.jupiter.api.Test;
+
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import io.hwc.enad.controller.HelloController;
 
 @SpringBootTest
 class EnadApplicationTests {
 
+	@Autowired
+	private HelloController helloController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		
+		Assert.assertEquals("Hello World from ENAD",helloController.home() );
 	}
 
 }
