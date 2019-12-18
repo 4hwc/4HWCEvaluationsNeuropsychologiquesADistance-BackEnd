bbed9c57f90f3595e5a602609ece33e212d4014d
Fanon Jupkwo <jupsfan@gmail.com>
2019-12-18 21:49:27 +0100

    Update EnadApplication.java

--- src/main/java/io/hwc/enad/EnadApplication.java
+++ src/main/java/io/hwc/enad/EnadApplication.java
@@ -3,6 +3,13 @@ package io.hwc.enad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Fanon Jupkwo
+ * 
+ * 
+ */
+
 @SpringBootApplication
 public class EnadApplication {
 
