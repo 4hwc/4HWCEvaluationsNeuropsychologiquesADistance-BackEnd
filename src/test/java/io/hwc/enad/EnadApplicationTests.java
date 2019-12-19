0bed03aefc535a54dd1df10424191a88023b4476
Fanon Jupkwo <jupsfan@gmail.com>
2019-12-19 13:03:13 +0100

    Back to Spring Boot 2.1.8 Coniguration

--- src/test/java/io/hwc/enad/EnadApplicationTests.java
+++ src/test/java/io/hwc/enad/EnadApplicationTests.java
@@ -1,18 +1,17 @@
 package io.hwc.enad;
 
-
-
-
-
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import io.hwc.enad.controller.HelloController;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class EnadApplicationTests {
+public class EnadApplicationTests {
 
 	@Autowired
 	private HelloController helloController;
